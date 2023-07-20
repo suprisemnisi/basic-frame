b826824ee7aabb9b56597de7f66bf57a7d24960f
suprisemnisi <140027867+suprisemnisi@users.noreply.github.com>
2023-07-20 04:03:33 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author khens
+ */
+public class BasicFrameApp {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //declare variable
+        MyFirstFrame frame1;
+        //insstantiate the frame
+        frame1 =new MyFirstFrame();
+    }
+    
+}
